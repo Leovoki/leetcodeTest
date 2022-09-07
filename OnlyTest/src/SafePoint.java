64f5456971cb343942f22361b6f7ca18e52859cb
LeoYu <724378735@qq.com>
2022-09-07 20:52:19 +0800

    安全点问题

---
+++ OnlyTest/src/SafePoint.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SafePoint {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Map<Object, String> map = new HashMap<>();
+                for(int i = Integer.MAX_VALUE; i > 0; i--) {
+                        //System.out.println("。");
+                    //map.put(new Object(), String.valueOf(i));
+                }
+                System.out.println(Thread.currentThread().getName() + ":大循环结束");
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < 10000; i++) {
+
+                }
+                System.out.println("小循环");
+            }
+
+        });
+
+        t1.start();
+        t2.start();
+        Thread.sleep(10);
+        System.out.println("主线程结束");
+    }
+}
